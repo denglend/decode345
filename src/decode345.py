5bc61ebe139d0a105a97c3db4460d606937c0e1d
211217613 <rubenavazquez@gmail.com>
2017-04-27 11:09:23 -0700

    Add space between all operators to improve readability

--- src/decode345.py
+++ src/decode345.py
@@ -53,93 +53,93 @@ except OSError as oe:
 		raise
 
 with open(SETTINGS["FIFOName"], "rb") as f:
-	curstate=0		# 0 = waiting for bit .... 1 = mid-bit
-	curlen=0		# length of current stream of symbols
-	parsedmostrecent=0	# flag that end of transimission has been reached
-	curstr=""		# current bitstream
+	curstate = 0		# 0 = waiting for bit .... 1 = mid-bit
+	curlen = 0		# length of current stream of symbols
+	parsedmostrecent = 0	# flag that end of transimission has been reached
+	curstr = ""		# current bitstream
 	while True:
 		data = f.read(1)
 		if len(data) == 0:
 			print("Writer closed")
 			break
 		if curstate == 0:
-			if ord(data)>noisefloor:					#signals change of bitstate
-				if curlen >=1000/2 and parsedmostrecent==0:		#end of bitstream
-					curstr = re.sub(r'^_*','',curstr)		#remove potentially many extraneous leading lows
-					curstr = "_"+curstr				#add back a single leading low (sync pattern always starts with a low)
+			if ord(data) > noisefloor:					#signals change of bitstate
+				if curlen >= 1000/2 and parsedmostrecent == 0:		#end of bitstream
+					curstr = re.sub(r'^_*', '', curstr)		#remove potentially many extraneous leading lows
+					curstr = "_" + curstr				#add back a single leading low (sync pattern always starts with a low)
 					if len(curstr) == 127 or len(curstr) == 191:
 						curstr = curstr + "_"			#add back an ending low if needed
 					if len(curstr) == 192 and SETTINGS["Show192"]:
 						print("String length: 192")		# temp decoding 192 bit strings
 						bytes = [0,0,0,0,0,0,0,0,0,0,0,0]
-						for curbyte in range(0,12):
-							for curbit in range(0,8):
+						for curbyte in range(0, 12):
+							for curbit in range(0, 8):
 								bytes[curbyte] *= 2
-								if curstr[curbyte*16+curbit*2:curbyte*16+curbit*2+2] == "_-": 
-									bytes[curbyte] +=1
-						print ('['+','.join(format(x, '02X') for x in bytes)+']')			
+								if curstr[curbyte * 16 + curbit * 2 : curbyte * 16 + curbit * 2 + 2] == "_-": 
+									bytes[curbyte] += 1
+						print ('[' + ','.join(format(x, '02X') for x in bytes)+ ']')			
 					elif len(curstr) != 128:
 						if SETTINGS["Verbose"]:
-							print("String length: "+str(len(curstr)))
+							print("String length: " + str(len(curstr)))
 							print(curstr)
 					else:				
-						bytes = [0,0,0,0,0,0,0,0]
-						for curbyte in range(0,8):
-							for curbit in range(0,8):
+						bytes = [0, 0, 0, 0, 0, 0, 0, 0]
+						for curbyte in range(0, 8):
+							for curbit in range(0, 8):
 								bytes[curbyte] *= 2
-								if curstr[curbyte*16+curbit*2:curbyte*16+curbit*2+2] == "_-": #low-high transition = 1
-									bytes[curbyte] +=1						
-						device = (bytes[2]<<16) + (bytes[3] <<8) +  bytes[4]
+								if curstr[curbyte * 16 + curbit * 2 : curbyte * 16 + curbit * 2 + 2] == "_-": #low-high transition = 1
+									bytes[curbyte] += 1						
+						device = (bytes[2] << 16) + (bytes[3] << 8) +  bytes[4]
 						status = bytes[5]
 						#CRC Check
-						crc = crcfunc("".join(map(chr,bytes[2:6])))
-						if (crc & 0xFF) == bytes[7] and ((crc & 0xFF00)>>8) == bytes[6]:
+						crc = crcfunc("".join(map(chr, bytes[2 : 6])))
+						if (crc & 0xFF) == bytes[7] and ((crc & 0xFF00) >> 8) == bytes[6]:
 							if device in devicelist:
 								client.connect(SETTINGS["MQTTHostName"], 1883, 60)	
-								client.publish("/security/"+devicelist[device],"OPEN" if 1<<7 & status >0 else "CLOSED")
+								client.publish("/security/" + devicelist[device], "OPEN" if 1 << 7 & status > 0 else "CLOSED")
 								client.disconnect()
-							if SETTINGS["ShowPulseChecks"] or (1<<2 & status) ==0: 
+							if SETTINGS["ShowPulseChecks"] or (1 << 2 & status) == 0: 
 								if device in devicelist:
-									print(devicelist[device],end=" ")
+									print(devicelist[device], end = " ")
 								else:
-									print("Unknown Device "+format(device,'06X'),end=" ")
+									print("Unknown Device " + format(device, '06X'), end = " ")
 								for statusbit in statuslist:
-									if (status & statusbit)==0:
-										print(statuslist[statusbit][0],end="")
+									if (status & statusbit) == 0:
+										print(statuslist[statusbit][0], end = "")
 									else:
-										print(statuslist[statusbit][1],end="")
-								print ('['+','.join(format(x, '02X') for x in bytes)+']')
+										print(statuslist[statusbit][1], end = "")
+								print ('[' + ','.join(format(x, '02X') for x in bytes) + ']')
 						elif SETTINGS["Verbose"]:
 							# CRC Fail
-							print("CRC Fail: ",end="")
-							print ('['+','.join(format(x, '02X') for x in bytes)+']')
-					curstr=""
-					parsedmostrecent=1
+							print("CRC Fail: " , end = "")
+							print ('[' + ','.join(format(x, '02X') for x in bytes) + ']')
+					curstr = ""
+					parsedmostrecent = 1
 					if SETTINGS["Verbose"]:
-						for hc in range(0,10000):
-							if Stats_High[hc]>0:print(str(hc)+": "+str(Stats_High[hc]),end=" ")
+						for hc in range(0, 10000):
+							if Stats_High[hc] > 0: print(str(hc) + ": " + str(Stats_High[hc]), end = " ")
 						print("")
-				elif curlen>Run1Len:
-					curstr +="__"
-				elif curlen>Run0Len:
-					curstr+="_"
+				elif curlen > Run1Len:
+					curstr += "__"
+				elif curlen > Run0Len:
+					curstr += "_"
 				curstate = 1
 				curlen = 1
 			else:
 				curlen += 1
 		else:
-			if ord(data)>noiseceiling:
+			if ord(data) > noiseceiling:
 				curlen += 1
 			else:
-				if curlen>Run1Len:
-					curstr+="--"
-					parsedmostrecent=0
-				elif curlen>Run0Len:
-					curstr+="-"
-					parsedmostrecent=0
-				elif curlen>RunErrLen:		#interpret as an error
-					print("\n  short - "+str(curlen))
-					curstr=""
-				if curlen <= 10000 and SETTINGS["Verbose"]: Stats_High[curlen]+=1
-				curlen=0
-				curstate=0			#wait for next bit
+				if curlen > Run1Len:
+					curstr += "--"
+					parsedmostrecent = 0
+				elif curlen > Run0Len:
+					curstr += "-"
+					parsedmostrecent = 0
+				elif curlen > RunErrLen:		#interpret as an error
+					print("\n  short - " + str(curlen))
+					curstr = ""
+				if curlen <= 10000 and SETTINGS["Verbose"]: Stats_High[curlen] += 1
+				curlen = 0
+				curstate = 0			#wait for next bit
